45c2bf0e283724d3776557bd75c1620254a8e35e
akhilaryan <akhil.ion@gmail.com>
2014-08-18 20:34:11 +0100

    Now she will tell you what to do in the gym

--- gym.py
+++ gym.py
@@ -1,7 +1,6 @@
 import random
 
 x = random.randint(1,100)
-print x
 
 if x%2 == 0:
 	x="even"
@@ -17,5 +16,15 @@ else:
 
 if x==y:
 	print "You guessed it. Take some rest, no gym today"
+	
 else:
 	print "No running away from this buddy. Go to the gym"
+	z = raw_input('Do you want me to tell you what to do in the gym?')
+	if z=="yes" or z == "y":
+		a = random.randint(1,3)
+		if a == 1:
+			print "Chest + Tricep + Shoulders"
+		elif a == 2:
+			print "Back + Biceps + Legs"
+		else:
+			print "Cardio + Abs"
