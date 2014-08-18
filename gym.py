7dc7b76a3afe4a9979ff2c487dbeb447e838b410
akhilaryan <akhil.ion@gmail.com>
2014-08-18 22:45:34 +0100

    Optimized code and gave better names to variables

--- gym.py
+++ gym.py
@@ -1,30 +1,30 @@
 import random
 
-x = random.randint(1,100)
-
-if x%2 == 0:
-	x="even"
+secret_number = random.randint(1,100)
+if secret_number % 2 == 0:
+	secret_number = "even"
 else:
-	 x = "odd"
-y = raw_input('odd or even?')
-if y == "odd" or y == "o" and x == "odd":
-	x=y
-elif y == "even" or y == "e" and x == "even":
-	x=y
-else:
-	x!=y
+	secret_number = "odd"
+user_choice = raw_input('odd or even?')
 
-if x==y:
-	print "You guessed it. Take some rest, no gym today"
+if user_choice[0] == "o" and secret_number == "odd":
+	secret_number = user_choice
+elif user_choice[0]=="e" and secret_number == "even":
+	secret_number = user_choice
+else:
+	secret_number != user_choice
+
+if secret_number == user_choice:
+	print "You guessed it. Take some rest, no gym today."
 	
 else:
 	print "No running away from this buddy. Go to the gym"
-	z = raw_input('Do you want me to tell you what to do in the gym?')
-	if z=="yes" or z == "y":
-		a = random.randint(1,3)
-		if a == 1:
+	workout = raw_input('Do you want me to tell you what to do in the gym?')
+	if workout[0] == "y":
+		module = random.randint(1,3)
+		if module == 1:
 			print "Chest + Tricep + Shoulders"
-		elif a == 2:
+		elif module == 2:
 			print "Back + Biceps + Legs"
 		else:
 			print "Cardio + Abs"
