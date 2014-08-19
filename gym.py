11570e03b0eb6b8250af4ca65db9dc2a3f823bc4
akhilaryan <akhil.ion@gmail.com>
2014-08-19 16:29:06 +0100

    Added input validation

--- gym.py
+++ gym.py
@@ -1,23 +1,26 @@
 import random
+import sys
 
 secret_number = random.randint(1,100)
-if secret_number % 2 == 0:
-	secret_number = "even"
+actual_number = secret_number
+if actual_number % 2 == 0:
+	actual_number = "even"
 else:
-	secret_number = "odd"
+	actual_number = "odd"
+
 user_choice = raw_input('odd or even?')
 
-if user_choice[0] == "o" and secret_number == "odd":
-	secret_number = user_choice
-elif user_choice[0]=="e" and secret_number == "even":
-	secret_number = user_choice
-else:
-	secret_number != user_choice
+while user_choice[0] != "o" and user_choice[0]!="e":
+	print "Your input was invalid. Try Again."
+	user_choice = raw_input('odd or even?')
+if user_choice[0] == "o" and actual_number == "odd":
+	actual_number = user_choice
+elif user_choice[0]=="e" and actual_number == "even":
+	actual_number = user_choice
 
-if secret_number == user_choice:
+if actual_number == user_choice:
 	print "You guessed it. Take some rest, no gym today."
-	
-else:
+elif actual_number != user_choice:
 	print "No running away from this buddy. Go to the gym"
 	workout = raw_input('Do you want me to tell you what to do in the gym?')
 	if workout[0] == "y":
